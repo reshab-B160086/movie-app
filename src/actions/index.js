9a0ebad82465632f9ffc73e94093dfe532bb9cd5
unknown <b160086@nitsikkim.ac.in>
2022-02-07 23:16:07 +0530

    adding actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,14 @@
 //     type : "ADD_MOVIES",
 //     movies : []
 // }
+
+//action types
+export const ADD_MOVIES = "ADD_MOVIES";
+
+//action creators
+export function addMovies(movies){
+    return {
+        type : ADD_MOVIES,
+        movies : movies
+    }
+}
