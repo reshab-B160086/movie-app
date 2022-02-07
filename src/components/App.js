b746225a277f23208611499e21f4f58fdde527f0
unknown <b160086@nitsikkim.ac.in>
2022-02-07 23:34:32 +0530

    refactoring the state

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
       console.log(this.props.store.getState());
   }
   render(){
-      const movies = this.props.store.getState();
+      const {list} = this.props.store.getState();
       return (
         <div className="App">
           <Navbar />
@@ -29,8 +29,8 @@ class App extends React.Component {
                 <div className="tab">Favourites</div>
             </div>
             <div className='list'>
-              {movies.map( (movie, index) => (
-                <MovieCard movie ={movie} key ={index}/>
+              {list.map( (movie, index) => (
+                <MovieCard movie ={movie} key ={`movies-${index}`}/>
               ))}
             </div>
           </div>
